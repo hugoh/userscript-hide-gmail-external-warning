16002448635c431122bc14e832fe1fb189527658
Hugo Haas <hugoh@hugoh.net>
2021-05-27 18:51:42 -0500

    Formatted with Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,12 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "greasemonkey": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": 12
-    },
-    "rules": {
-    },
+  env: {
+    browser: true,
+    es2021: true,
+    greasemonkey: true,
+  },
+  extends: "eslint:recommended",
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {},
 };
