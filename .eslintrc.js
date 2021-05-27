108969f6101c847e75621502db8cfb1a77be0900
Hugo Haas <hugoh@hugoh.net>
2021-05-27 18:39:28 -0500

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "greasemonkey": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 12
+    },
+    "rules": {
+    },
+};
---
+++ hide-gmail-external-warning.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Hide external recipients warnings in Google Workspace's Gmail
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  For cases when you cannot follow https://support.google.com/a/answer/7380041?hl=en
+// @author       Hugo Haas
+// @include      https://mail.google.com/mail/*
+// @grant        GM_addStyle
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    GM_addStyle('div.aau {display: none;}');
+})();
