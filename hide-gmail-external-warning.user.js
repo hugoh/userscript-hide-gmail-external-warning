3611ef468e1d790d7ba81835cce76b0019b6ce99
Hugo Haas <hugoh@hugoh.net>
2021-05-27 19:01:36 -0500

    Added source metadata + README

--- hide-gmail-external-warning.user.js
+++ hide-gmail-external-warning.user.js
@@ -1,7 +1,9 @@
 // ==UserScript==
 // @name         Hide external recipients warnings in Google Workspace's Gmail
-// @namespace    http://tampermonkey.net/
+// @namespace    https://github.com/hugoh/
 // @version      0.1
+// @source       https://github.com/hugoh/userscript-hide-gmail-external-warning
+// @downloadURL  https://github.com/hugoh/userscript-hide-gmail-external-warning/raw/main/hide-gmail-external-warning.user.js
 // @description  For cases when you cannot follow https://support.google.com/a/answer/7380041?hl=en
 // @author       Hugo Haas
 // @include      https://mail.google.com/mail/*
